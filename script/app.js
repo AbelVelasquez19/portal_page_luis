8f03f068dbffa2ae734e61f50d096a5ec523ad39
abel.velasquez1997@gmail.com <abel.velasquez1997@gmail.com>
2022-01-18 17:16:16 -0500

    inicio de Nav bar

---
+++ script/app.js
@@ -0,0 +1,9 @@
+const displayNoneLogoWepPc = document.querySelector('.logo_web_pc');
+const displayNoneLogoWepMovil = document.querySelector('.logo_web_movil');
+const displayNoneIcons= document.querySelector('.social-profile');
+const  logo_control = ()=>{
+    displayNoneLogoWepPc.style.display='none';
+    displayNoneIcons.style.display='none';
+}
+
+logo_control();
