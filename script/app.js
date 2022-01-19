0e7605c542b603101b64ba8d3830b05dd202848d
Abel Velasquez <abel.velasquez1997@gmail.com>
2022-01-18 23:02:42 -0500

    Pagina Inicio tipo Mobile First terminado

--- script/app.js
+++ script/app.js
@@ -1,9 +1,21 @@
 const displayNoneLogoWepPc = document.querySelector('.logo_web_pc');
 const displayNoneLogoWepMovil = document.querySelector('.logo_web_movil');
-const displayNoneIcons= document.querySelector('.social-profile');
+const displayNoneNav_link= document.querySelector('.nav_link');
+const menu_btn = document.querySelector("#menu-btn");
+
 const  logo_control = ()=>{
     displayNoneLogoWepPc.style.display='none';
-    displayNoneIcons.style.display='none';
+   // displayNoneNav_lin.style.display='none';
 }
 
 logo_control();
+
+menu_btn.onclick=()=>{
+    menu_btn.classList.toggle('fa-times');
+    displayNoneNav_link.classList.toggle('active');
+}
+
+window.onscroll = ()=>{
+    menu_btn.classList.remove('fa-times');
+    displayNoneNav_link.classList.remove('active');
+}
